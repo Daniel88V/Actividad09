dc3a80f2fe79161fa18425cc6707f0e4c05c36fa
Daniel88V <daniel12velo@gmail.com>
2025-07-23 19:11:51 -0600

    Agencia de viajes

--- Agencia.py
+++ Agencia.py
@@ -1,8 +1,8 @@
 clientes = {}
-destinos = []
 print("¿Cuántos clientes desea ingresar?: ")
 cant = int(input())
 for i in range(cant):
+    destinos = []
     print(f"Cliente #{i +1}: ")
     while True:
         codigo = input("Ingrese el código del cliente: ")
@@ -28,4 +28,18 @@ for i in range(cant):
         }
     }
 
-print("¿Desea realizar alguna de las siguientes ")
+print("¿Desea realizar alguna de las siguientes opciones?")
+print("1. Desplegar listado de clientes")
+print("2. Desplegar el total de destinos visitados")
+print("3. Desplegar el cliente con más destinos visitados")
+print("4. Salir")
+print("Seleccione una opción: ")
+opcion = input()
+if opcion == "1":
+    print("\t ---Listado de clientes---")
+    for codigo, datos in clientes.items():
+        print(f"Codigo: {codigo}")
+        print(f"Nombre: {datos['nombre']}")
+        print(f"Destinos: {datos['destino']['lugares']}")
+elif opcion == "2":
+    print("\t ---Cantidad de destinos visitados---")
