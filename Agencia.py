0a944e32701b7bb1e3c1e34109b76f9656ee5105
Daniel88V <daniel12velo@gmail.com>
2025-07-23 18:50:30 -0600

    Agencia de viajes

--- Agencia.py
+++ Agencia.py
@@ -1,4 +1,5 @@
 clientes = {}
+destinos = []
 print("¿Cuántos clientes desea ingresar?: ")
 cant = int(input())
 for i in range(cant):
@@ -11,15 +12,19 @@ for i in range(cant):
         else:
             break
     clientes[codigo]["nombre"] = input("Ingrese el nombre del cliente: ")
+    """
     clientes[codigo]["edad"] = int(input("Ingrese el edad del cliente: "))
     clientes[codigo]["telefono"] = input("Ingrese el telefono del cliente: ")
     clientes[codigo]["correo"] = input("Ingrese el correo del cliente: ")
+    """
     print("¿Cuántos destinos ha visitado el cliente?: ")
     dest = int(input())
     while True:
-        if dest > 0:
+        if 0 < dest <= 5 :
             for j in range(dest):
-                print(f"Destino #{j+1}: ")
-
+                visitas = input(f"Destino #{j+1}: ")
+                destinos.append(visitas)
+            break
         else:
-            print("La cantidad de destinos debe ser mayor a 0")
+            print("La cantidad de destinos debe ser mayor a 0 y menor que 6")
+print("¿Desea realizar alguna de las siguientes")
